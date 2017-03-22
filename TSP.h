161a50ca2e03595f242441d4fe1527247ff31c96
Martin Damov <damov.martin@gmail.com>
2017-03-22 19:36:18 +0000

    Improvements to TSP brute force

---
+++ TSP.h
@@ -0,0 +1,14 @@
+#ifndef TSP_H
+#define TSP_H
+
+#include "stdafx.h"
+#include <iostream>
+#include "edge.h"
+#include "graph.h"
+
+//TODO: Return the path from TSP_brute_force
+
+double TSP_brute_force(Graph &graph);
+
+
+#endif // TSP
