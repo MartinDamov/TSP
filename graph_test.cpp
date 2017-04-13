e999635fbe380128a19391b2841e435f03c03123
Martin Damov <damov.martin@gmail.com>
2017-04-13 17:21:25 +0100

    Improvements to make it more interactive

--- graph_test.cpp
+++ graph_test.cpp
@@ -7,62 +7,37 @@
 #include "TSP.h"
 #include "TSP_branch_and_bound.h"
 
-int main() {
-
+/**
+	An example showing how the program works. All the output is displayed to the console
+*/
+void showExample() {
+	
 	// Construct graphs, add edges to them and display them in console
-	Graph graph1(4);
-	graph1.AddEdge(1, 2, 10);
-	graph1.AddEdge(1, 3, 15);
-	graph1.AddEdge(1, 4, 20);
-	graph1.AddEdge(2, 3, 35);
-	graph1.AddEdge(2, 4, 25);
-	graph1.AddEdge(3, 4, 30);
+	Graph graph(3);
+	graph.AddEdge(1, 2, 10);
+	graph.AddEdge(2, 3, 15);
+	
+	std::cout << "The following graph will be used to demonstrate the methods:" << std::endl;
+	graph.display();
 
-	// Display graph1
-	graph1.display();
-
-	// Empty line for seperation
+	// Seperation line
 	std::cout << std::endl;
 
-	Graph graph2(5);
-	graph2.AddEdge(1, 2, 10);
-	graph2.AddEdge(1, 3, 11);
-	graph2.AddEdge(1, 4, 12);
-	graph2.AddEdge(2, 3, 11);
-	graph2.AddEdge(2, 4, 13);
-	graph2.AddEdge(3, 4, 12);
-	graph2.AddEdge(4, 5, 13);
-	graph2.AddEdge(3, 5, 14);
-	graph2.AddEdge(2, 5, 15);
-
-	Graph ham(5);
-	ham.AddEdge(1, 2, 2);
-	ham.AddEdge(1, 4, 4);
-	ham.AddEdge(2, 3, 3);
-	ham.AddEdge(2, 4, 4);
-	ham.AddEdge(2, 5, 5);
-	ham.AddEdge(3, 5, 5);
-	ham.AddEdge(4, 5, 5);
-
-	ham.display();
-
-	std::cout << std::endl;
-
-	double dis = TSP_brute_force(ham);
-	std::cout << "TSP brute force distance: " << dis << std::endl;
-	std::vector<int> pathbrute = TSP_brute_force_path();
+	// Brute force
+	double distance_example = TSP_brute_force(graph);
+	std::cout << "TSP brute force distance: " << distance_example << std::endl;
+	std::vector<int> path_example = TSP_brute_force_path();
 	std::cout << "TSP brute force path: ";
-	for (int i = 0; i < pathbrute.size(); i++) {
-		std::cout << pathbrute[i] << " ";
+	for (int i = 0; i < path_example.size(); i++) {
+		std::cout << path_example[i] << " ";
 	}
 
-	// Seperation line
+	// Seperation lines
+	std::cout << std::endl;
 	std::cout << std::endl;
 
-	// Seperation line
-	std::cout << std::endl;
-
-	TSP_branch_and_bound_set_graph(ham);
+	// Branch and bound 
+	TSP_branch_and_bound_set_graph(graph);
 	double distance = TSP_branch_and_bound_distance();
 	std::cout << "TSP branch and bound: " << distance << std::endl;
 	std::cout << "TSP branch and bound path: ";
@@ -73,11 +48,96 @@ int main() {
 
 	// Seperation line
 	std::cout << std::endl;
-
-	// Seperation line
 	std::cout << std::endl;
 
 	// Add pause so the console doesn't terminate
+	std::cout << "Press ENTER to continue... ";
+	std::cin.ignore();
+}
+
+int main() {
+	
+	char answer;
+	std::cout << "Do you want to see an example? (y/n)" << std::endl;
+	std::cin >> answer;
+	if (answer == 'y') {
+		showExample();
+	}
+	else {
+		std::cout << "The next steps are for inputing a graph." << std::endl;
+		
+		// Input a graph by the console
+		Graph graph = Graph();
+
+		std::cout << "The inputed graph is: " << std::endl;
+		graph.display();
+
+		// Seperation line
+		std::cout << std::endl;
+
+		int method;
+		std::cout << "Choose a method - brute force (1) or branch and bound(2)" << std::endl;
+		std::cin >> method;
+		if (method < 2) {
+			// Call brute force
+			double distance_brute_force = TSP_brute_force(graph);
+			std::cout << "TSP brute force distance: " << distance_brute_force << std::endl;
+			std::vector<int> path_brute_force = TSP_brute_force_path();
+			std::cout << "TSP brute force path: ";
+			for (int i = 0; i < path_brute_force.size(); i++) {
+				std::cout << path_brute_force[i] << " ";
+			}
+
+			char answer;
+			std::cout << "\nWould you like to see the other method on the same graph? (y/n)" << std::endl;
+			std::cin >> answer;
+			if (answer == 'y') {
+				// Call branch and bound
+				TSP_branch_and_bound_set_graph(graph);
+				double distance_branch_and_bound = TSP_branch_and_bound_distance();
+				std::cout << "TSP branch and bound: " << distance_branch_and_bound << std::endl;
+				std::cout << "TSP branch and bound path: ";
+				std::vector<int> path_branch_and_bound = TSP_branch_and_bound_path();
+				for (auto i = 0; i < path_branch_and_bound.size(); i++) {
+					std::cout << path_branch_and_bound[i] << " ";
+				}
+			}
+
+			// Add pause so the console doesn't terminate
+			std::cin.ignore();
+		}
+		else {
+			// Call branch and bound
+			TSP_branch_and_bound_set_graph(graph);
+			double distance_branch_and_bound = TSP_branch_and_bound_distance();
+			std::cout << "TSP branch and bound: " << distance_branch_and_bound << std::endl;
+			std::cout << "TSP branch and bound path: ";
+			std::vector<int> path_branch_and_bound = TSP_branch_and_bound_path();
+			for (auto i = 0; i < path_branch_and_bound.size(); i++) {
+				std::cout << path_branch_and_bound[i] << " ";
+			}
+
+			char answer;
+			std::cout << "\nWould you like to see the other method on the same graph? (y/n)" << std::endl;
+			std::cin >> answer;
+			if (answer == 'y') {
+				// Call brute force
+				double distance_brute_force = TSP_brute_force(graph);
+				std::cout << "TSP brute force distance: " << distance_brute_force << std::endl;
+				std::vector<int> path_brute_force = TSP_brute_force_path();
+				std::cout << "TSP brute force path: ";
+				for (int i = 0; i < path_brute_force.size(); i++) {
+					std::cout << path_brute_force[i] << " ";
+				}
+			}
+
+			// Add pause so the console doesn't terminate
+			std::cin.ignore();
+		}
+	}
+
+	// Add pause so the console doesn't terminate
+	std::cout << std::endl;
 	std::cout << "Press ENTER to continue... ";
 	std::cin.ignore();
 
