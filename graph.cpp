4cc3ad6d5a768daeb00b9d1f718383d72d473755
Martin Damov <damov.martin@gmail.com>
2017-05-04 22:57:09 +0100

    Update to read_graph

--- graph.cpp
+++ graph.cpp
@@ -3,8 +3,10 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <sstream>
 #include "graph.h"
 #include "edge.h"
+#include "city.h"
 
 
 Graph::Graph(const int size) {
@@ -34,8 +36,118 @@ Graph::Graph(Graph &other) {
 	}
 }
 
-Graph::~Graph() {
-	std::cout << "~Graph() was invoked" << std::endl;
+
+// Manual input for a graph via the console.
+Graph::Graph() {
+	int size;
+	std::cout << "Enter number of vertices: ";
+	std::cin >> size;
+	size_ = size;
+
+	// Fill the adjacency matrix with zeros
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			adjacency_matrix_[i][j] = 0;
+		}
+	}
+
+	for (int i = 0; i < size*size; i++) {
+		int v1;
+		int v2;
+		double weight;
+		std::cout << "Enter edge (in the form of: v1, v2) and weight (-1 -1 -1 to exit): ";
+		std::cin >> v1 >> v2 >> weight;
+		if (v1 <= 0) {
+			break;
+		}
+		if (v1 > 0 && v2 > 0 && weight >= 0.1) {
+
+			// Add the edge
+			adjacency_matrix_[v1 - 1][v2 - 1] = weight;
+			adjacency_matrix_[v2 - 1][v1 - 1] = weight;
+		}
+	}
+}
+
+/**
+Read a graph file in TSPLIB format.
+Get as parameter a file name (as well as the format of the file, for example: test.txt)
+*/
+Graph::Graph(std::string name) {
+
+	// Open file
+	std::ifstream myfile(name);
+	std::string line;
+
+	int size = -1, vertex;
+	double xCord, yCord;
+	std::vector<City> cities;
+	char a;
+	bool read_cities = false;
+
+	if (!myfile.fail()) {
+		while (std::getline(myfile, line)) {
+			if (line.substr(0, 9) == "DIMENSION") {
+				// Get size
+				size = stoi(line.substr(11));
+			}
+
+			if (read_cities) {
+				std::stringstream stream(line);
+				stream >> vertex >> xCord >> yCord;
+				cities.push_back(City(vertex, xCord, yCord));
+			}
+
+			if (line.substr(0, 10) == "NODE_COORD") {
+				read_cities = true;
+			}
+		}// end while
+
+		myfile.close();
+	}// end outer if
+
+	else {
+		std::cout << "Error! File could not be opened!" << std::endl;
+		myfile.close();
+		abort();
+	}
+
+	if (size != -1) {
+		size_ = size;
+
+		// Fill the adjacency matrix with zeros
+		for (int i = 0; i < size_; i++) {
+			for (int j = 0; j < size_; j++) {
+				adjacency_matrix_[i][j] = 0;
+			}
+		}
+
+		// Add edges
+		for (auto i = 0; i < cities.size()-1; i++) {
+			//double dis = calculate_distance(cities[i].getXCord(), cities[i].getYCord(), cities[i+1].getXCord(), cities[i+1].getXCord());
+			double dis = pow((cities[i].getXCord() - cities[i].getYCord()), 2) + pow((cities[i + 1].getXCord() - cities[i + 1].getXCord()), 2);
+			// Add the edge
+			std::cout << cities[i].getXCord() << " " <<  cities[i].getYCord() << std::endl;
+			std::cout << sqrt(dis) << std::endl;
+			//adjacency_matrix_[cities[i].getNumber() - 1][cities[i+1].getNumber()- 1] = dis;
+			//adjacency_matrix_[cities[i+1].getNumber() - 1][cities[i].getNumber() - 1] = dis;
+
+		}
+		
+	}
+	else {
+		std::cout << "Error! Size not defined in file!" << std::endl;
+		abort();
+	}
+
+} // end read graph
+
+  /*
+  Calculate distance between two coordinate points
+  */
+double calculate_distance(double xCord_v1, double yCord_v1, double xCord_v2, double yCord_v2) {
+	double distance = pow((xCord_v1 + yCord_v1), 2) + pow((xCord_v2 + yCord_v2), 2);
+	return distance;
 }
 
 const int Graph::size() const { return size_; }
@@ -96,6 +208,7 @@ std::vector<Edge> Graph::GetAllEdges() const
 
 Graph &Graph::operator=(Graph &other) {
 	size_ = other.size();
+	size_ = other.size();
 	Graph graph(size_);
 	int v1 = 0;
 	int v2 = 0;
@@ -109,103 +222,3 @@ Graph &Graph::operator=(Graph &other) {
 	}
 	return graph;
 }
-
-// Manual input for a graph via the console.
-Graph::Graph() {
-	int size;
-	std::cout << "Enter number of vertices: ";
-	std::cin >> size;
-	size_ = size;
-
-	// Fill the adjacency matrix with zeros
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
-			adjacency_matrix_[i][j] = 0;
-		}
-	}
-
-	for (int i = 0; i < size; i++) {
-		int v1;
-		int v2;
-		double weight;
-		std::cout << "Enter edge (in the form of: v1, v2) and weight (-1 -1 -1 to exit): ";
-		std::cin >> v1 >> v2 >> weight;
-		if (v1 <= 0) {
-			break;
-		}
-		if (v1 > 0 && v2 > 0 && weight >= 0.1) {
-			
-			// Add the edge
-			adjacency_matrix_[v1 - 1][v2 - 1] = weight;
-			adjacency_matrix_[v2 - 1][v1 - 1] = weight;
-		}
-	}
-}
-
-/**
-	Read a graph file in DIMACS format.
-	Get as parameter a file name (as well as the format of the file, for example: test.txt or graph.gr)
-*/
-Graph::Graph(std::string name) {
-
-	// Open file
-	std::ifstream myfile(name);
-	std::string line;
-
-	int size = -1, v1, v2, weight;
-	std::vector<Edge> edges;
-	char a;
-
-	if (!myfile.fail()) {
-		while (getline(myfile, line)) {
-			if (line.substr(0, 1) == "p") {
-				// Get size
-				size = stoi(line.substr(2, 1));
-			}
-			else if (line.substr(0, 1) == "a") {
-				// Get edges and add them to the vector
-				myfile >> a >> v1 >> v2 >> weight;
-				edges.push_back(Edge(v1, v2, weight));
-				std::cout << v1 << " " << v2 << " " << weight << std::endl;
-			}
-		}// end while
-
-		myfile.close();
-	}// end outer if
-	else {
-		std::cout << "Error! File could not be opened!" << std::endl;
-		abort();
-	}
-
-	if (size != -1) {
-		size_ = size;
-
-
-		// Fill the adjacency matrix with zeros
-		for (int i = 0; i < size; i++) {
-			for (int j = 0; j < size; j++) {
-				adjacency_matrix_[i][j] = 0;
-			}
-		}
-
-		// Add edges
-		for (auto i = 0; i < edges.size(); i++) {
-			int v1 = edges[i].getVertex1();
-			int v2 = edges[i].getVertex2();
-			double weight = edges[i].getWeight();
-			/*
-			// Add the edge
-			adjacency_matrix_[v1 - 1][v2 - 1] = weight;
-			adjacency_matrix_[v2 - 1][v1 - 1] = weight;
-			*/
-			std::cout << "adding: " << v1 << " " << v2 << " " << weight << std::endl;
-		}
-
-
-	}
-	else {
-		std::cout << "Error! Size not defined in file!" << std::endl;
-		abort();
-	}
-
-} // end read graph
