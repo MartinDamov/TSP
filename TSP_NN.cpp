350b50af94ec8e82c8258d6b488c5f4b481a4f4e
Martin Damov <damov.martin@gmail.com>
2017-05-06 08:36:27 +0100

    Minor improvments

--- TSP_NN.cpp
+++ TSP_NN.cpp
@@ -1,42 +1,49 @@
 #include "stdafx.h"
 #include <vector>
 #include <algorithm>
-#include <set>
 #include "graph.h"
 #include "TSP_NN.h"
 
+
 double TSP_nearest_neighbour(Graph &graph) {
-	double distance = 0;
-	std::set<int> visited_vertices;
+	double distance = 0.0;
+	std::vector<int> visited;
 	std::vector<int> tour;
-	int current_vertex = 3; // This is the start vertex
-	bool start = true;
-	visited_vertices.insert(current_vertex);
+	int start_vertex = 0;
+	int next_vertex;
+	int current_vertex = start_vertex; // This is the start vertex
+	double min = std::numeric_limits<double>::max();
 	tour.push_back(current_vertex);
 
-	//while(visited_vertices.size() < graph.size()) {
-	for (int j = 0; j < graph.size(); j++){
-		int next_vertex = 0;
+	int number_of_vertices = graph.size();
+
+	for (int i = 0; i < number_of_vertices; i++) {
+
+		// The vertex has been visited
+		visited.push_back(current_vertex);
 		double min = std::numeric_limits<double>::max();
+		next_vertex = 0;
 
 		// Find nearest vertex
-		for (int i = 0; i < graph.size(); i++) {
-
-			if (i != current_vertex && graph.GetEdgeWeight(current_vertex, i) < min) {
-				// Check if the vertex has been visited already (0 used for the start
-				if (visited_vertices.find(i) != visited_vertices.end()) {
-					next_vertex = i;
-					visited_vertices.insert(next_vertex);
-					distance += graph.GetEdgeWeight(current_vertex, next_vertex);
-					current_vertex = next_vertex;
-					min = graph.GetEdgeWeight(current_vertex, i);
-					tour.push_back(next_vertex);
+		for (int j = 0; j < number_of_vertices; j++) {
+			if (j != current_vertex && std::find(visited.begin(), visited.end(), j) == visited.end()) {
+				// Vertex has not been visited and it is not the current one
+				if (min > graph.GetEdgeWeight(current_vertex, j)) {
+					min = graph.GetEdgeWeight(current_vertex, j);
+					next_vertex = j;
 				}
 			}
-		}		
+		}
+
+		// Add the vertex to the tour
+		tour.push_back(next_vertex);
+		// Calculate the distance
+		distance += graph.GetEdgeWeight(current_vertex, next_vertex);
+		// Set the current vertex
+		current_vertex = next_vertex;
 	}
 
-	std::cout << "Tour: ";
+	std::cout << "\nTour: ";
 	for (int i = 0; i < tour.size(); i++) {
 		std::cout << tour[i] << " ";
 	}
